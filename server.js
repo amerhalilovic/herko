76bddf75824f6aa9d5328bc31b7919d9089f592f
amerhalilovic <amer.malibu@gmail.com>
2019-05-29 13:13:49 +0200

    safafa

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname + '/public/index.html'))
+    res.sendFile(path.join(__dirname + '/public/menu.html'))
 })
 
 app.listen(PORT);
