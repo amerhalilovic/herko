bd9a3e2eb7f436f778e7e45e65b9b0ff56c591f5
amerhalilovic <amer.malibu@gmail.com>
2019-05-29 12:28:07 +0200

    safafafa

--- server.js
+++ server.js
@@ -5,21 +5,7 @@ const path = require('path')
 const app = express();
 const PORT = process.env.PORT;
 
-app.get("/", (req, res) => {
 
-    res.send("Hello");
-})
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
-
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname + '/public/index.html'))
-})
 
 app.listen(PORT);
 
