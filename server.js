7ce8d55707d8b4e530cec6fe1162cda39b96b849
amerhalilovic <amer.malibu@gmail.com>
2019-05-29 12:19:03 +0200

    sada

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser') 
+const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT;
@@ -6,6 +8,17 @@ const PORT = process.env.PORT;
 app.get("/", (req, res) => {
 
     res.send("Hello");
+})
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname + '/public/index.html'))
 })
 
 app.listen(PORT);
